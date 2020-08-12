e967334659f35bd39beac6359be4fddec2371a07
Sarvesh Patil <sarveshp273@gmail.com>
2020-08-12 11:01:26 +0530

    Scroll smoothened

--- js/home.js
+++ js/home.js
@@ -1,10 +1,12 @@
 $(window).scroll(function(){
     if ($(this).scrollTop() > 20) {
-      $('.svg-1').css("visibility","visible");
+      // $('.svg-1').css("visibility","visible");
+      $('.svg-1').css("display","block");
       $('.svg-1').css("position","fixed");
       $('.svg-1').css("z-index","1000000");
     }else{
-      $('.svg-1').css("visibility","hidden");
+      // $('.svg-1').css("visibility","hidden");
+      $('.svg-1').css("display","none");
       $('.svg-1').css("position","");
       $('.svg-1').css("z-index","");
     }
